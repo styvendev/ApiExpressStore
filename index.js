7905ec313ddcba8344b6c38125cbce5db6a54910
styvensk8 <stivencastro636@gmail.com>
2022-03-09 17:35:27 -0500

    add router

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ app.get('/', (req, res) => {
   res.send('server in express');
 });
 
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'PC',
+      price: 1500000,
+    },
+    {
+      name: 'Audifonos',
+      price: 650000,
+    },
+  ]);
+});
+
+app.get('/categories/:category/product/:product', (req, res) => {
+  const { category, product } = req.params;
+  res.json({
+    id: 125,
+    category,
+    product,
+    price: 10000000,
+  });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay ningun parametro');
+  }
+});
+
 app.listen(port, () => {
   console.log('port run on serve', 'localhost:' + port);
 });
